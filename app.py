14f671cf950d87a670645c176fcb6d3a7c78d2bb
vaibhavkumarrrr <156523525+vaibhavkumarrrr@users.noreply.github.com>
2024-01-23 02:43:07 +0000

    EXP MEIN TRY 1

--- app.py
+++ app.py
@@ -2,26 +2,23 @@ from flask import Flask, render_template
 
 main = Flask(__name__)
 EXP = [{
-    "company": "Career Mantra",
+    "company": "Career IN Mantra",
     "postion": "Social media marketing intern",
     "location": "Remote",
-    "duration": 3
-}, {
-    "company": "Nex gen media",
+    "duration": 3}, 
+    {"company": "Nex gen media",
     "postion": "SMM",
     "location": "Remote",
-    "duration": 1
-}, {
-    "company": "NOVA INNOVATIVE COMPSKEY",
-    "postion": "Creative marketing lead",
-    "location": "Bengaluru",
-    "duration ": 29
-}]
+    "duration": },
+    {"company":"mvj college of enginnering",
+    "position": "student",
+    "location": "bengaluru",
+    "duration":24      }]
 
 
 @main.route('/')
 def index():
-  return render_template("home.html", expr=EXP, fname="vk")
+  return render_template('home.html', expr=EXP, fname="ANJALI")
 
-
-main.run(host='0.0.0.0', port=81, debug=True)
+if __name__ == "__main__":
+  main.run(host='0.0.0.0', port=81, debug=True)
